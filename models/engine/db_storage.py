ec151c77bca664fd060a0a6e5707de2337e39212
electrikbox <electrikbox@icloud.com>
2023-12-01 11:03:35 +0100

    back to old storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,12 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
-from models import *
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class DBStorage():
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models import *
 
 
 class FileStorage:
@@ -9,22 +8,30 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
+    # All
+    # Updated to return the list of objects of one type of class
+    # ============================================================ #
+
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        if cls is not None:
-            return {
-                key: value for key, value in FileStorage.__objects.items()
-                if isinstance(value, cls)
-                }
-        else:
+        """Returns a dict or filtered list of models currently in storage"""
+        if cls is None:
             return FileStorage.__objects
+        else:
+            return {key: obj for key, obj in FileStorage.__objects.items()
+                    if isinstance(obj, cls)}
+
+    # New
+    # ============================================================ #
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
+    # Save
+    # ============================================================ #
+
     def save(self):
-        """Saves storage dictionary to file"""
+        """ Saves storage dictionary to file """
         with open(FileStorage.__file_path, 'w') as f:
 
             temp = {
@@ -34,6 +41,36 @@ class FileStorage:
 
             json.dump(temp, f, indent=4)
 
+    # Reload
+    # ============================================================ #
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
+
+    # Delete
+    # ============================================================ #
+
     def delete(self, obj=None):
         """ Deletes an object from the storage dict """
         if obj is None:
@@ -42,20 +79,3 @@ class FileStorage:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            data = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-                for key, val in data.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
