9e1e316322ba9bad9a736b3b514ef8f4a0227724
electrikbox <electrikbox@icloud.com>
2023-12-11 21:21:27 +0100

    add comment

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -83,4 +83,5 @@ class FileStorage:
     # Close
     # ============================================================ #
     def close(self):
+        """Calls reload() method for deserializing the JSON file to objects"""
         self.reload()
